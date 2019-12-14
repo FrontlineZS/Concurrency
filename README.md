9a98eefae60006aca35c80f07e210b883893df5e
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-12-14 15:00:26 +0100

    initial commit __ contains new created project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Creating clear, correct and well documented concurrent programs
---
+++ src/main/java/com/frontline/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,18 @@
+package com.frontline.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Creating clear, correct and well documented concurrent programs
+ * by FrontlineZS
+ */
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+
+    }
+
+}
