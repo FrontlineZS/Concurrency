ff8c7541550ae45df9023784eefc35b1e89a0ced
Kochaniuk Radosław <radoslaw.kochaniuk@assecods.pl>
2020-07-23 23:23:42 +0200

    formatting task

--- src/main/java/com/frontline/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/frontline/concurrency/ConcurrencyApplication.java
@@ -1,18 +1,26 @@
 package com.frontline.concurrency;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 /**
  * Creating clear, correct and well documented concurrent programs
  * by FrontlineZS
  */
-@SpringBootApplication
 public class ConcurrencyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConcurrencyApplication.class, args);
+        try (final Scanner scanner = new Scanner(System.in)) {
+            printSeparatingLine();
 
+            for (int i = 0; i < 3; i++) {
+                System.out.printf("%-15s%03d\n", scanner.next(), scanner.nextInt());
+            }
+
+            printSeparatingLine();
+        }
     }
 
+    private static void printSeparatingLine() {
+        System.out.println("================================");
+    }
 }
